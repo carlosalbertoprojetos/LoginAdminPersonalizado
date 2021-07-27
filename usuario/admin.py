4f13867b9b8b992543695fd4a8f01205d4ce8ea3
carlosalbertoprojetos <carlosalbertoprojetos2020@gmail.com>
2021-07-27 12:07:08 -0300

    customizando admin

--- usuario/admin.py
+++ usuario/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .forms import UserAdminCreationForm, UserAdminChangeForm
 
 User = get_user_model()
 
-admin.site.register(User)
+class UserAdmin(BaseUserAdmin):
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
 
-# Register your models here.
+    list_display = ['email', 'admin','profile' ]
+    list_filter = ['admin','profile','staff', 'is_active',]
+    fieldsets = (
+        (None, {'fields': ('profile','email', 'password',)}),
+        ('Personal info', {'fields': ()}),
+        ('Permissions', {'fields': ('admin','staff', 'is_active')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'password_2')}
+        ),
+    )
+    search_fields = ['email', 'is_staff', 'is_active', ]
+    ordering = ['email']
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+
